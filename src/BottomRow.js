119286c49cf3fb268a7f80c946990d81438e6966
Gerard Crockenberg <gmahred@gmail.com>
2019-12-10 09:51:32 -0500

    quarter counter

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  const [quarter, quarterCounter] = useState(1)
+
   return (
     <div className="bottomRow">
       <div className="down">
@@ -18,7 +20,13 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{quarter}</div>
+      </div>
+      <div className="homeButtons">
+        <button className="homeButtons_touchdown" onClick={() => quarterCounter(quarter + 1)}>Quarter Add</button>
+      </div>
+      <div className="homeButtons">
+        <button className="homeButtons_touchdown" onClick={() => quarterCounter(quarter / quarter)}>Quarter Reset</button>
       </div>
     </div>
   );
